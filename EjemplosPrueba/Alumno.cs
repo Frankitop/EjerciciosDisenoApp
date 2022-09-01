ef449e8f8e0b387f9f32f7b28336b0f12eba20ca
Frankitop <fpiriz88@gmail.com>
2022-08-31 23:54:58 -0300

    first commit

---
+++ EjemplosPrueba/Alumno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemplosPrueba
+{
+    public class Alumno
+    {
+        private int numero;
+        private string nombres;
+        private string apellidos;
+        private DateTime fechaNacimiento;
+
+        public int GetNumero()
+        {
+            return this.numero;
+        }
+
+        public void SetNumero(int unNumero)
+        {
+            this.numero = unNumero;
+        }
+    }
+}
---
+++ EjemplosPrueba/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemplosPrueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno unAlumno = new Alumno();
+            unAlumno.SetNumero(123456);
+
+            Console.WriteLine(
+                string.Format("Se creo el alumno con numero {0}", unAlumno.GetNumero()));
+            Console.ReadLine();
+        }
+    }
+}
